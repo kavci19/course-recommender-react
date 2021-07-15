401bce4226a4d1de24651052a9295fb06868b5e9
Kaan Avci <kaanyewest1@gmail.com>
2021-07-15 19:40:37 -0400

    Added spinner for loading while waiting for API response

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {useState, useEffect} from 'react'
 import axios from 'axios';
 import Alert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles({
   subjectLine: {
@@ -12,14 +13,20 @@ const useStyles = makeStyles({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row'
-
   },
+  center: {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+ }
 });
 
 function App() {
 
   const [department, setDepartment] = useState([])
   const [courseList, setCourseList] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const classes = useStyles();
 
@@ -29,6 +36,7 @@ function App() {
       
       // const url = "http://127.0.0.1:5000/courses/"
       const url = "https://course-rec-api.herokuapp.com/courses/"
+      setLoading(true)
       var {data} = await axios.get(url, {
         params: {
           deps: department
@@ -36,6 +44,7 @@ function App() {
       })
       data = data['data']
 
+      setLoading(false)
       setCourseList(data)
 
     }
@@ -55,15 +64,18 @@ console.log(department)
         <SubjectPicker setDepartment = {setDepartment}/>
         <SubjectPickerHelper/>
         </div>
+        <div className={classes.center}>
+          {loading ? (<CircularProgress />) : ""}
+        </div>
+
+        
+        
         {courseList.length === 0 &&
                   
            <Alert severity="error">No top-rated courses found. Select a different department!</Alert>
        
         }
 
-        
-
-        
         <Courses courseList = {courseList} department = {department}/>
     </div>
   );
